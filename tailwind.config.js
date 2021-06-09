90081a5c8192da6fc1940e397a63600a0d111a9c
HassanHabibTahir <47211929+HassanHabibTahir@users.noreply.github.com>
2021-06-09 11:06:50 +0500

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ['top', 'left'],
+    },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-debug-screens'),
+
+  ],
+}
